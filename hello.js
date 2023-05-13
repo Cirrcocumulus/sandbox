92bb1b18d6f030baf3c148dff8098b25fc3ada2c
Cirrcocumulus <133458927+Cirrcocumulus@users.noreply.github.com>
2023-05-13 02:14:48 +0000

    Began work on number guessing game

---
+++ hello.js
@@ -0,0 +1,16 @@
+<script>
+let randomNumber = Math.floor(Math.random()*100)+ 1;
+
+const guesses = document.queryselector('.guesses');
+const lastResult = document.queryselector('.lastResult');
+const loworHi = document.queryselector('.loworHi');
+
+const guessSubmit = document.queryselector('.guessSubmit');
+const guessField = document.queryselector('.guessField');
+
+let guessCount = 1'
+let resetButton;
+
+
+
+</script>
